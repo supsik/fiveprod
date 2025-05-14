7bf85ee7382cc69e499b77eb24d584a830bbc0e6
Krutoi Chuvak <hasboliev@gmail.com>
2025-05-14 17:54:34 +0300

    project created

---
+++ components/preloader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="preloader">
+    <h1>пятыйкруг.прод</h1>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss" scoped>
+.preloader {
+  width: 100%;
+  height: 100vh;
+  font-family: Doloman;
+  display: flex;
+
+  h1 {
+    display: block;
+    position: relative;
+    z-index: 2;
+    margin: auto;
+    font-size: 108px;
+    font-weight: 400;
+
+    &::after {
+      width: 215px;
+      height: 135px;
+      right: 0;
+      top: 0;
+      position: absolute;
+      z-index: 1;
+      display: block;
+      content: '';
+      background-image: url('/public/img/pigeon.png');
+    }
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class='main-frame'>
+    <preloader />
+  </div>
+</template>
+<script setup>
+</script>
+<style lang="scss" scoped>
+</style>
